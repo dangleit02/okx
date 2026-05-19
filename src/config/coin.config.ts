992eb5d7ecd2373f9cfd36c83a119ba2e9ea0e35
danglemediconcen <dang_le@mediconcen.com>
2026-05-19 07:50:30 +0700

    update

--- src/config/coin.config.ts
+++ src/config/coin.config.ts
@@ -7,7 +7,7 @@ export default () => ({
     runSpotTaskHavingStopLoss: false,
     runSwapTaskForShort: false,
     runSwapTaskForLong: false,
-    maxUsdt: 3000,
+    maxUsdt: 4000,
     riskPerTrade: 0.02,
     amountOfUsdtPerStep: 12,
     stopLossRatio: 0.01, // 1%
@@ -510,8 +510,8 @@ export default () => ({
             addForTriggerPrice: 0.000000001, // Đơn vị giá để tính giá kích hoạt
             szToFixed: 3, // Số chữ số thập phân cho khối lượng
             priceToFixed: 9, // Số chữ số thập phân cho giá
-            minBuyPriceRatio: 3,
-            maxBuyPriceRatio: 4,
+            minBuyPriceRatio: 0.03,
+            maxBuyPriceRatio: 0.04,
         },
         WIF: {
             maxUsdt: 1500, // Số tiền tối đa để đầu tư cho mỗi coin (USDT)
@@ -662,8 +662,8 @@ export default () => ({
             addForTriggerPrice: 0.0001, // Đơn vị giá để tính giá kích hoạt
             szToFixed: 4, // Số chữ số thập phân cho khối lượng
             priceToFixed: 4, // Số chữ số thập phân cho giá
-            minBuyPriceRatio: 2,
-            maxBuyPriceRatio: 3,
+            minBuyPriceRatio: 0.02,
+            maxBuyPriceRatio: 0.03,
         },
         XLM: {
             maxUsdt: 1500, // Số tiền tối đa để đầu tư cho mỗi coin (USDT)
@@ -796,8 +796,8 @@ export default () => ({
             addForTriggerPrice: 0.000001, // Đơn vị giá để tính giá kích hoạt
             szToFixed: 6, // Số chữ số thập phân cho khối lượng
             priceToFixed: 2, // Số chữ số thập phân cho giá
-            minBuyPriceRatio: 1,
-            maxBuyPriceRatio: 2,
+            minBuyPriceRatio: 0.01,
+            maxBuyPriceRatio: 0.02,
         },
         XRP: {
             maxUsdt: 1500, // Số tiền tối đa để đầu tư cho mỗi coin (USDT)
